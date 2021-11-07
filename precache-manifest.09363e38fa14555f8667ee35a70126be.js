d9e482c1384a23a73fa4de20471ef2dc31aca878
Rehman-Kabir <rehmankabir786@gmail.com>
2021-11-07 06:10:03 +0500

    Updates

---
+++ precache-manifest.09363e38fa14555f8667ee35a70126be.js
@@ -0,0 +1,498 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "914d07a7369742e9e7bd728606d93e4b",
+    "url": "/matx-pro/index.html"
+  },
+  {
+    "revision": "6703a7a9515219e86e5a",
+    "url": "/matx-pro/static/css/19.cabd1a9b.chunk.css"
+  },
+  {
+    "revision": "f1845f82f7c636709b88",
+    "url": "/matx-pro/static/css/26.edc2ba64.chunk.css"
+  },
+  {
+    "revision": "53c5ca372d2d8fb3b994",
+    "url": "/matx-pro/static/css/main.b11c43d5.chunk.css"
+  },
+  {
+    "revision": "456b0c64c1fd15c855b4",
+    "url": "/matx-pro/static/js/0.2c6dc1fa.chunk.js"
+  },
+  {
+    "revision": "1f3eb94b6696eddf8373",
+    "url": "/matx-pro/static/js/1.a2a375b1.chunk.js"
+  },
+  {
+    "revision": "fd7525d544dd9c67d07855cb8778e590",
+    "url": "/matx-pro/static/js/1.a2a375b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dce80f5e8e365155206",
+    "url": "/matx-pro/static/js/10.ffa1b3a5.chunk.js"
+  },
+  {
+    "revision": "a4585dad937be7661d43",
+    "url": "/matx-pro/static/js/100.ead7744b.chunk.js"
+  },
+  {
+    "revision": "522e05b8951c8172c46a",
+    "url": "/matx-pro/static/js/101.d5dd789a.chunk.js"
+  },
+  {
+    "revision": "fbd46250462124945cc6",
+    "url": "/matx-pro/static/js/102.30633885.chunk.js"
+  },
+  {
+    "revision": "a0c89ba97ac7eb662283",
+    "url": "/matx-pro/static/js/103.786840f1.chunk.js"
+  },
+  {
+    "revision": "5c93b304f4b93b4cf143",
+    "url": "/matx-pro/static/js/104.43691436.chunk.js"
+  },
+  {
+    "revision": "695bc13718cd95cfd31f",
+    "url": "/matx-pro/static/js/105.8f1d0be7.chunk.js"
+  },
+  {
+    "revision": "8cb1843dc21b061c1b0a",
+    "url": "/matx-pro/static/js/106.0163bf41.chunk.js"
+  },
+  {
+    "revision": "f2230e07a5e7bb0e9f9e",
+    "url": "/matx-pro/static/js/107.21ec61e7.chunk.js"
+  },
+  {
+    "revision": "ac930c8e05732cd78b54",
+    "url": "/matx-pro/static/js/108.408a0e57.chunk.js"
+  },
+  {
+    "revision": "da057de33c9f2dda152c",
+    "url": "/matx-pro/static/js/109.3ecf0f3b.chunk.js"
+  },
+  {
+    "revision": "cbfcf146886e54a7777c",
+    "url": "/matx-pro/static/js/11.1d0faf83.chunk.js"
+  },
+  {
+    "revision": "72a707cbde6eaa88cfff",
+    "url": "/matx-pro/static/js/12.f77a46a7.chunk.js"
+  },
+  {
+    "revision": "0b512ce367777ca994d8",
+    "url": "/matx-pro/static/js/13.5134c711.chunk.js"
+  },
+  {
+    "revision": "0b7feb18c9277ad3c8bc",
+    "url": "/matx-pro/static/js/14.1358ac42.chunk.js"
+  },
+  {
+    "revision": "f9024049818297e41186",
+    "url": "/matx-pro/static/js/15.4e6d32b6.chunk.js"
+  },
+  {
+    "revision": "a6d3d2164f343d779a86",
+    "url": "/matx-pro/static/js/16.c2b2e3a8.chunk.js"
+  },
+  {
+    "revision": "6703a7a9515219e86e5a",
+    "url": "/matx-pro/static/js/19.1fec8089.chunk.js"
+  },
+  {
+    "revision": "c4795edb8a57d824fa60f2a0b455e489",
+    "url": "/matx-pro/static/js/19.1fec8089.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7240e0960dcb3bed3bc",
+    "url": "/matx-pro/static/js/2.02c76ffc.chunk.js"
+  },
+  {
+    "revision": "cfc573f003c513320ad3",
+    "url": "/matx-pro/static/js/20.4e894e7c.chunk.js"
+  },
+  {
+    "revision": "4567137a76b3c6a5e801",
+    "url": "/matx-pro/static/js/21.0d73c4e2.chunk.js"
+  },
+  {
+    "revision": "888711f882f7c4edf33c",
+    "url": "/matx-pro/static/js/22.e3a85c83.chunk.js"
+  },
+  {
+    "revision": "3fe3474907a6a3fddc83",
+    "url": "/matx-pro/static/js/23.b8308dbc.chunk.js"
+  },
+  {
+    "revision": "028707b3e5820d784a9c",
+    "url": "/matx-pro/static/js/24.cc32517c.chunk.js"
+  },
+  {
+    "revision": "b94b0ae724e25cf351b3",
+    "url": "/matx-pro/static/js/25.66a71cdc.chunk.js"
+  },
+  {
+    "revision": "5c9b10f97fb4ef33b40be2abacc4f956",
+    "url": "/matx-pro/static/js/25.66a71cdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1845f82f7c636709b88",
+    "url": "/matx-pro/static/js/26.80aeac06.chunk.js"
+  },
+  {
+    "revision": "0b54f0919f6f9178ad05f36fb0d28ceb",
+    "url": "/matx-pro/static/js/26.80aeac06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0b684ac81bdfb531a52",
+    "url": "/matx-pro/static/js/27.d8c8f9ea.chunk.js"
+  },
+  {
+    "revision": "ebee0f06a78b8c9a1913",
+    "url": "/matx-pro/static/js/28.068934d3.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "/matx-pro/static/js/28.068934d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99776eda976a0846f7ab",
+    "url": "/matx-pro/static/js/29.69f13754.chunk.js"
+  },
+  {
+    "revision": "0106dae60ccc8e6b3664",
+    "url": "/matx-pro/static/js/3.7410474d.chunk.js"
+  },
+  {
+    "revision": "708fa3854dbc778a3c90",
+    "url": "/matx-pro/static/js/30.58d5c1e4.chunk.js"
+  },
+  {
+    "revision": "66c775d3225dcd803d35",
+    "url": "/matx-pro/static/js/31.ad80cfd3.chunk.js"
+  },
+  {
+    "revision": "166ec95120a94623b0ac",
+    "url": "/matx-pro/static/js/32.7e2e0146.chunk.js"
+  },
+  {
+    "revision": "45f0150759c45f66e1af",
+    "url": "/matx-pro/static/js/33.337be2fc.chunk.js"
+  },
+  {
+    "revision": "fb073e5bf99ee1c37129",
+    "url": "/matx-pro/static/js/34.efa46cdf.chunk.js"
+  },
+  {
+    "revision": "f8ad1d8f4209361ffac0",
+    "url": "/matx-pro/static/js/35.4412d327.chunk.js"
+  },
+  {
+    "revision": "862753428bad8d12b945",
+    "url": "/matx-pro/static/js/36.a8c2fe0d.chunk.js"
+  },
+  {
+    "revision": "bfaa1f74f280ba4af2b8",
+    "url": "/matx-pro/static/js/37.f7422c35.chunk.js"
+  },
+  {
+    "revision": "f3bdf0d071aa7d547446",
+    "url": "/matx-pro/static/js/38.a8f91617.chunk.js"
+  },
+  {
+    "revision": "7f15eb33ba141b594394",
+    "url": "/matx-pro/static/js/39.fb2dabe5.chunk.js"
+  },
+  {
+    "revision": "add1a58d2823c196ac55",
+    "url": "/matx-pro/static/js/4.8ff6f453.chunk.js"
+  },
+  {
+    "revision": "9e689bbdeca81c9f8b67",
+    "url": "/matx-pro/static/js/40.e95af5c8.chunk.js"
+  },
+  {
+    "revision": "77d84e937315fc3137fd",
+    "url": "/matx-pro/static/js/41.2baa9780.chunk.js"
+  },
+  {
+    "revision": "16799220aade435937ac",
+    "url": "/matx-pro/static/js/42.da86ca68.chunk.js"
+  },
+  {
+    "revision": "24774435fc2c01deedc9",
+    "url": "/matx-pro/static/js/43.b84a2f18.chunk.js"
+  },
+  {
+    "revision": "25f4d1d670054a5a9705",
+    "url": "/matx-pro/static/js/44.5bc11bd3.chunk.js"
+  },
+  {
+    "revision": "3feaf2ab5c7ef305a7f0",
+    "url": "/matx-pro/static/js/45.34ebdd49.chunk.js"
+  },
+  {
+    "revision": "27d5062bc417d2bbea4f",
+    "url": "/matx-pro/static/js/46.c8192a0f.chunk.js"
+  },
+  {
+    "revision": "fcf73e25909447aa5d7a",
+    "url": "/matx-pro/static/js/47.03588b68.chunk.js"
+  },
+  {
+    "revision": "447466ae09c6de06e662",
+    "url": "/matx-pro/static/js/48.52d0a9a6.chunk.js"
+  },
+  {
+    "revision": "615ee07452a80badfe2e",
+    "url": "/matx-pro/static/js/49.4216f2dc.chunk.js"
+  },
+  {
+    "revision": "44c8a0b3b4a99934c7df",
+    "url": "/matx-pro/static/js/5.11cb6dbd.chunk.js"
+  },
+  {
+    "revision": "3bf54910f06046559589",
+    "url": "/matx-pro/static/js/50.f106bf02.chunk.js"
+  },
+  {
+    "revision": "2ef46b52d5a88f8ff41b",
+    "url": "/matx-pro/static/js/51.0d06a5a0.chunk.js"
+  },
+  {
+    "revision": "5f4f372cd390a88bae39",
+    "url": "/matx-pro/static/js/52.c7a0866b.chunk.js"
+  },
+  {
+    "revision": "e80ed66295b3ad398286",
+    "url": "/matx-pro/static/js/53.60db4f34.chunk.js"
+  },
+  {
+    "revision": "d5982abaf18ebcf30d27",
+    "url": "/matx-pro/static/js/54.ce3eaaa0.chunk.js"
+  },
+  {
+    "revision": "4718a1e9911f28c9ad28",
+    "url": "/matx-pro/static/js/55.0afc9943.chunk.js"
+  },
+  {
+    "revision": "0d5a5af52e76980dfec0",
+    "url": "/matx-pro/static/js/56.9bdfd759.chunk.js"
+  },
+  {
+    "revision": "3e296479dc0147f17d15",
+    "url": "/matx-pro/static/js/57.8303a578.chunk.js"
+  },
+  {
+    "revision": "b0106a72cca5436714e9",
+    "url": "/matx-pro/static/js/58.ab68f841.chunk.js"
+  },
+  {
+    "revision": "b9cffe7dc3e1eef976e25492d4674723",
+    "url": "/matx-pro/static/js/58.ab68f841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e51c20dcc107e4f21c83",
+    "url": "/matx-pro/static/js/59.f9201cb2.chunk.js"
+  },
+  {
+    "revision": "7b9b46272647ef2650b2",
+    "url": "/matx-pro/static/js/6.58d09cdc.chunk.js"
+  },
+  {
+    "revision": "acee29fa711900726e8a",
+    "url": "/matx-pro/static/js/60.80f65234.chunk.js"
+  },
+  {
+    "revision": "a986186117f9dcfa47b0",
+    "url": "/matx-pro/static/js/61.3e5bf2a6.chunk.js"
+  },
+  {
+    "revision": "78258b05509fcc7fa304",
+    "url": "/matx-pro/static/js/62.1e14e39a.chunk.js"
+  },
+  {
+    "revision": "87f7e1d39f5a331c7f7c",
+    "url": "/matx-pro/static/js/63.27d6bc2e.chunk.js"
+  },
+  {
+    "revision": "b757c2a0abd4728a6e4a",
+    "url": "/matx-pro/static/js/64.b1a6239c.chunk.js"
+  },
+  {
+    "revision": "ee1464e50af7add67fac",
+    "url": "/matx-pro/static/js/65.30b543c6.chunk.js"
+  },
+  {
+    "revision": "cfd9f08a038cdf0695b8",
+    "url": "/matx-pro/static/js/66.99d544e7.chunk.js"
+  },
+  {
+    "revision": "6edf876bf3e0a00a946d",
+    "url": "/matx-pro/static/js/67.eded838f.chunk.js"
+  },
+  {
+    "revision": "cc79a63d920736450b9f",
+    "url": "/matx-pro/static/js/68.c7a5d48d.chunk.js"
+  },
+  {
+    "revision": "ae55b53fad0790a521b6",
+    "url": "/matx-pro/static/js/69.3aedd1c3.chunk.js"
+  },
+  {
+    "revision": "fa6a792e8f43db621f5b",
+    "url": "/matx-pro/static/js/7.2c968df9.chunk.js"
+  },
+  {
+    "revision": "aa49201be8313e80fc33",
+    "url": "/matx-pro/static/js/70.49d65df9.chunk.js"
+  },
+  {
+    "revision": "7f8a0df12ed2a882df52",
+    "url": "/matx-pro/static/js/71.710a7685.chunk.js"
+  },
+  {
+    "revision": "8d54b45f3dd90a58374d",
+    "url": "/matx-pro/static/js/72.8398a357.chunk.js"
+  },
+  {
+    "revision": "1fa7bd8a7ecd67308a57",
+    "url": "/matx-pro/static/js/73.e0123476.chunk.js"
+  },
+  {
+    "revision": "88d31fcacf80b8116050",
+    "url": "/matx-pro/static/js/74.88aaaa4b.chunk.js"
+  },
+  {
+    "revision": "84c2d7393ddff361509c",
+    "url": "/matx-pro/static/js/75.06a4275e.chunk.js"
+  },
+  {
+    "revision": "a5cd8ee5683ff598da50",
+    "url": "/matx-pro/static/js/76.8d694fca.chunk.js"
+  },
+  {
+    "revision": "d46958e022b65d76717d",
+    "url": "/matx-pro/static/js/77.d967bffd.chunk.js"
+  },
+  {
+    "revision": "2b1f0cf56eb34db9346d",
+    "url": "/matx-pro/static/js/78.e3f2dc5d.chunk.js"
+  },
+  {
+    "revision": "9d5e1cb439111acb6e98",
+    "url": "/matx-pro/static/js/79.ab9d4347.chunk.js"
+  },
+  {
+    "revision": "9c7c8912990821b371ac",
+    "url": "/matx-pro/static/js/8.172a7606.chunk.js"
+  },
+  {
+    "revision": "4a18c09f9f3dd7d85345",
+    "url": "/matx-pro/static/js/80.2f4a6afd.chunk.js"
+  },
+  {
+    "revision": "db87dda0c10d6721ee1d",
+    "url": "/matx-pro/static/js/81.a5b307dc.chunk.js"
+  },
+  {
+    "revision": "37a484fc7bb71d007f52",
+    "url": "/matx-pro/static/js/82.f091b6f5.chunk.js"
+  },
+  {
+    "revision": "91554ac55e523258d27c",
+    "url": "/matx-pro/static/js/83.275a82d5.chunk.js"
+  },
+  {
+    "revision": "de6c0026ff97e9cd2d54",
+    "url": "/matx-pro/static/js/84.cc323788.chunk.js"
+  },
+  {
+    "revision": "65637f8d8d5bc839fc95",
+    "url": "/matx-pro/static/js/85.d4f9e7e7.chunk.js"
+  },
+  {
+    "revision": "31040bc08a8c808d5e1d",
+    "url": "/matx-pro/static/js/86.2aaf0f44.chunk.js"
+  },
+  {
+    "revision": "b6969431d2c39b42a98c",
+    "url": "/matx-pro/static/js/87.a032ea78.chunk.js"
+  },
+  {
+    "revision": "a3411701dee695032059",
+    "url": "/matx-pro/static/js/88.81bf26b4.chunk.js"
+  },
+  {
+    "revision": "99bfc67ed5d22ba82be6",
+    "url": "/matx-pro/static/js/89.84f9364a.chunk.js"
+  },
+  {
+    "revision": "76c9c6b394fd0d1bd8e3",
+    "url": "/matx-pro/static/js/9.9427d435.chunk.js"
+  },
+  {
+    "revision": "614c146538186a852ababdb53abf501a",
+    "url": "/matx-pro/static/js/9.9427d435.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce820b6256ae57c616e7",
+    "url": "/matx-pro/static/js/90.0061ade5.chunk.js"
+  },
+  {
+    "revision": "181e99e9a442f441cdb6",
+    "url": "/matx-pro/static/js/91.0a4a0efc.chunk.js"
+  },
+  {
+    "revision": "c56143753e8a30268466",
+    "url": "/matx-pro/static/js/92.e0333637.chunk.js"
+  },
+  {
+    "revision": "bb5345746e3da7eaf99e",
+    "url": "/matx-pro/static/js/93.539895c9.chunk.js"
+  },
+  {
+    "revision": "abbb2aede4decb21672c",
+    "url": "/matx-pro/static/js/94.5777cb7f.chunk.js"
+  },
+  {
+    "revision": "2e8bade826d2bb0087e6",
+    "url": "/matx-pro/static/js/95.bf62d971.chunk.js"
+  },
+  {
+    "revision": "521600170596c07aef9e",
+    "url": "/matx-pro/static/js/96.03b4953b.chunk.js"
+  },
+  {
+    "revision": "883b3417f545639c876b",
+    "url": "/matx-pro/static/js/97.75983408.chunk.js"
+  },
+  {
+    "revision": "5f0397af1eec7d4e9c1d",
+    "url": "/matx-pro/static/js/98.53699edb.chunk.js"
+  },
+  {
+    "revision": "34bd63950b5ae519c7a2",
+    "url": "/matx-pro/static/js/99.c5e3e6b4.chunk.js"
+  },
+  {
+    "revision": "53c5ca372d2d8fb3b994",
+    "url": "/matx-pro/static/js/main.7b6074f0.chunk.js"
+  },
+  {
+    "revision": "dd305da164af5fc0b131",
+    "url": "/matx-pro/static/js/runtime-main.180c30ee.js"
+  },
+  {
+    "revision": "280d9ddc8f17ea225a226dc33205182a",
+    "url": "/matx-pro/static/media/BwModelicaSS01-Regular.280d9ddc.otf"
+  },
+  {
+    "revision": "8246cddab3351da521ae9b32ec0191a4",
+    "url": "/matx-pro/static/media/pexels-breston-kenya-5214278.8246cdda.png"
+  },
+  {
+    "revision": "6f26d4eac366a6f56d7032713bca222e",
+    "url": "/matx-pro/static/media/sitelogo.6f26d4ea.svg"
+  }
+]);
